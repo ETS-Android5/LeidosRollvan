417cd9686ec2aee5f8240c84926999e910f854df
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2021-11-17 14:19:57 +0000

    Modified footer
    Added 4th destination to the footer,changed names for exisiting  footer links.
    Map footer icon doesn't link to any page yet. Changed corresponding packages, ids and all methods to account for the new names

--- Android_Project/app/src/main/java/com/example/leidosrollvan/ui/browse/BrowseFragment.kt
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/ui/browse/BrowseFragment.kt
@@ -1,4 +1,4 @@
-package com.example.leidosrollvan.ui.notifications
+package com.example.leidosrollvan.ui.browse
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.leidosrollvan.R
-import com.example.leidosrollvan.databinding.FragmentNotificationsBinding
+import com.example.leidosrollvan.databinding.FragmentBrowseBinding
 
-class NotificationsFragment : Fragment() {
+class BrowseFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var browseViewModel: BrowseViewModel
+    private var _binding: FragmentBrowseBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        browseViewModel =
+            ViewModelProvider(this).get(BrowseViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentBrowseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textDashboard
+        browseViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- Android_Project/app/src/main/java/com/example/leidosrollvan/ui/browse/BrowseViewModel.kt
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/ui/browse/BrowseViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.leidosrollvan.ui.notifications
+package com.example.leidosrollvan.ui.browse
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BrowseViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
