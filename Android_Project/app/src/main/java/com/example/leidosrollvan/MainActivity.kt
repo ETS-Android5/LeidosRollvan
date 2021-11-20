f329af10598408e75a4f366e55172ac5ac7420de
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2021-11-20 19:24:29 +0000

    Fixed and recreated broken app template
    closes #51
    Reset the template of the app to use a blank empty activity template and created
    the bottom navigation bar from scratch. The Bottom navigation bar now opens
    each page upon clicking on the icons, and the icon labels are always visible.
    The app builds successfully, indicating no broken features.
    
    NOTE: Utilise the "build > clean Project" option in android studio to fix
    most bugs encountered when building the app

--- Android_Project/app/src/main/java/com/example/leidosrollvan/MainActivity.kt
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.leidosrollvan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.leidosrollvan.fragments.AccountFragment
+import com.example.leidosrollvan.fragments.HomeFragment
+import com.example.leidosrollvan.fragments.MapFragment
+import com.example.leidosrollvan.fragments.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val mapFragment = MapFragment()
+    private val accountFragment = AccountFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(homeFragment)
+
+        bottom_navigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.id_home -> replaceFragment(homeFragment)
+                R.id.id_search -> replaceFragment(searchFragment)
+                R.id.id_map -> replaceFragment(mapFragment)
+                R.id.id_account -> replaceFragment(accountFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
