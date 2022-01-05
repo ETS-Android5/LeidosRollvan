e934f72b63ecf131546139f0df0103b1405afe33
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2022-01-05 17:01:22 +0000

    Create Business Registration branch
    Created business registration, login and home activity files.

---
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/BusinessHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.leidosrollvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusinessHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_home);
+    }
+}
