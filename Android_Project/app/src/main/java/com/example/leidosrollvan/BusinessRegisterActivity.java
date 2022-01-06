fe05da236cfb6ad7a22ac8a4e2b1a8749c900a4e
Kieran Sharp <2134299s@student.ac.uk>
2022-01-06 13:53:19 +0000

    business register page edited

--- Android_Project/app/src/main/java/com/example/leidosrollvan/BusinessRegisterActivity.java
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/BusinessRegisterActivity.java
@@ -1,16 +1,25 @@
 package com.example.leidosrollvan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 
@@ -21,11 +30,15 @@ public class BusinessRegisterActivity extends AppCompatActivity implements View.
     private ProgressBar businessProgressBar;
     private CircularProgressButton registerBusiness;
 
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_register);
         changeStatusBarColor();
+
+        mAuth = FirebaseAuth.getInstance();
 
         existingBusinessAccount = (TextView) findViewById(R.id.existingBusinessAccount);
         existingBusinessAccount.setOnClickListener(this);
@@ -56,7 +69,78 @@ public class BusinessRegisterActivity extends AppCompatActivity implements View.
     }
 
     public void registerBusiness(){
-        startActivity(new Intent(this, BusinessHomeActivity.class));
+        //startActivity(new Intent(this, BusinessHomeActivity.class));
+        String businessEmail = editBusinessTextEmail.getText().toString().trim();
+        String businessName = editBusinessTextName.getText().toString().trim();
+        String businessMobile = editBusinessTextMobile.getText().toString().trim();
+        String businessPassword = editBusinessTextPassword.getText().toString().trim();
+
+        if(businessName.isEmpty()){
+            editBusinessTextName.setError("Name is Required!");
+            editBusinessTextName.requestFocus();
+            return;
+        }
+
+        if(businessMobile.isEmpty()){
+            editBusinessTextMobile.setError("Mobile Number is Required!");
+            editBusinessTextMobile.requestFocus();
+            return;
+        }
+
+        if(businessEmail.isEmpty()){
+            editBusinessTextEmail.setError("Email is Required!");
+            editBusinessTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(businessEmail).matches()){
+            editBusinessTextEmail.setError("Please provide valid email!");
+            editBusinessTextEmail.requestFocus();
+            return;
+        }
+
+        if(businessPassword.isEmpty()){
+            editBusinessTextPassword.setError("Password is Required!");
+            editBusinessTextPassword.requestFocus();
+            return;
+        }
+
+        if(businessPassword.length() < 6){
+            editBusinessTextPassword.setError("Minimum password length should be 6 characters");
+            editBusinessTextPassword.requestFocus();
+            return;
+        }
+        businessProgressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(businessEmail, businessPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(businessName, businessMobile, businessEmail);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(BusinessRegisterActivity.this, "Business has been registered successfully", Toast.LENGTH_LONG).show();
+                                        businessProgressBar.setVisibility(View.GONE);
+                                        startActivity(new Intent(BusinessRegisterActivity.this, LoginActivity.class));
+
+                                    }else {
+                                        Toast.makeText(BusinessRegisterActivity.this, "Failed to register Business, try again!", Toast.LENGTH_LONG).show();
+                                        businessProgressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }else {
+                            Toast.makeText(BusinessRegisterActivity.this, "Failed to register business, try again!", Toast.LENGTH_LONG).show();
+                            businessProgressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
     }
 
     @Override
