16a6fd43e3cd1ba6c527c3cf9c069c4d71d7a9ab
alex <2558782c@student.gla.ac.uk>
2022-02-19 00:36:07 +0000

    Allow item name input to contain punctuation as previously it would have caused an error.

--- Android_Project/app/src/main/java/com/example/leidosrollvan/adapters/businessItemRecyclerAdapter.java
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/adapters/businessItemRecyclerAdapter.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.leidosrollvan.R;
+import com.example.leidosrollvan.staticClasses.InputValidation;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -51,6 +52,7 @@ public class businessItemRecyclerAdapter extends RecyclerView.Adapter<businessIt
         HashMap<String, String> item = itemList.get(position);
         String name = item.keySet().toArray()[0].toString();
         String price = "£"+item.get(name);
+        name = InputValidation.decodeFromFirebaseKey(name);
         holder.foodName.setText(name);
         holder.foodPrice.setText(price);
     }
