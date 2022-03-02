d4f885fca79b03c027ca03ccbc0682f37dd73435
alex <2558782c@student.gla.ac.uk>
2022-03-02 15:31:07 +0000

    Change colors of business home page to match color scheme and remove edit image text on business settings page.

--- Android_Project/app/src/main/java/com/example/leidosrollvan/activity/BusinessHomeActivity.java
+++ Android_Project/app/src/main/java/com/example/leidosrollvan/activity/BusinessHomeActivity.java
@@ -22,6 +22,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -77,6 +78,8 @@ public class BusinessHomeActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //force light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_home);
 
